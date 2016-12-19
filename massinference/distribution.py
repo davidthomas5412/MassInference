ebe9902c366d1a963da805e60b0aced76fb714c3
David Thomas <davidthomas5412@gmail.com>
2016-12-19 02:34:36 -0800

    permutes true distrubtion

--- massinference/distribution.py
+++ massinference/distribution.py
@@ -4,15 +4,18 @@
 """
 TODO
 """
-from numpy.random import uniform
+from numpy.random import permutation
 
 
 class MassPrior(object):
     """
     TODO
     """
-    def sample(self, size=1):
-        return uniform(10**10, 10**15, size)
+    def __init__(self, prior):
+        self.prior = prior
+
+    def sample(self, size=1): #TODO: revisit size
+        return permutation(self.prior)
 
 # class Distribution(object):
 #     def __init(self):
