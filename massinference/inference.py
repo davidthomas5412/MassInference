81edb801e317741372b74d3f87acf1d56f188317
David Thomas <davidthomas5412@gmail.com>
2016-12-19 02:35:32 -0800

    vectorized log-likelihood

--- massinference/inference.py
+++ massinference/inference.py
@@ -4,10 +4,10 @@
 """
 TODO
 """
-from numpy import mean, sqrt, log, pi, sum
+from numpy import mean, sqrt, log, pi, sum, zeros
 
 
-def log_likelihood(g, e1, e2, sigma_e):
+def log_likelihood(predictions, e1, e2, sigma_e):
     """
         Compare observed ellipticity to ray traced reduced shear.
     """
@@ -17,12 +17,14 @@ def log_likelihood(g, e1, e2, sigma_e):
     sigma = sqrt(sigma_e ** 2 + std_obs ** 2)
 
     # Calculate the (log of) normalization constant
-    samples = 2. * len(g)
+    samples = 2. * len(predictions)
     log_z = (samples / 2.) * log(2. * pi * sigma ** 2)
+    log_likelihood = zeros(len(predictions))
 
-    # Calculate chi2
-    chi2 = sum((e1 - g.real) ** 2 / sigma ** 2) + sum((e2 - g.imag) ** 2 / sigma ** 2)
-
-    # Calculate log-likelihood
-    log_likelihood = -log_z + (-0.5) * chi2
+    for i in xrange(len(predictions)):
+        g = predictions[i]
+        # Calculate chi2
+        chi2 = sum((e1 - g.real) ** 2 / sigma ** 2) + sum((e2 - g.imag) ** 2 / sigma ** 2)
+        # Calculate log-likelihood
+        log_likelihood[i] = -log_z + (-0.5) * chi2
     return log_likelihood
